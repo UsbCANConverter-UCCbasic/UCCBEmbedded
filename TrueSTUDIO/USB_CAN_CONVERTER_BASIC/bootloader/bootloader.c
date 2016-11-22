26ef31a7abe43e53ba2c42a021b75266265bd5a7
Lukasz Juranek <lukasz.juranek@pl.abb.com>
2016-11-22 16:16:22 +0100

    Bootlader update
    Bootlader update

--- TrueSTUDIO/USB_CAN_CONVERTER_BASIC/bootloader/bootloader.c
+++ TrueSTUDIO/USB_CAN_CONVERTER_BASIC/bootloader/bootloader.c
@@ -1,61 +1,54 @@
 /*
- * bootloader.c
- * taken from topping.will from st forum(https://my.st.com/public/STe2ecommunities/mcu/Lists/cortex_mx_stm32/Flat.aspx?RootFolder=%2Fpublic%2FSTe2ecommunities%2Fmcu%2FLists%2Fcortex%5Fmx%5Fstm32%2FJump%20to%20USB%20DFU%20Bootloader%20in%20startup%20code%20on%20STM32F042&FolderCTID=0x01200200770978C69A1141439FE559EB459D7580009C4E14902C3CDE46A77F0FFD06506F5B&currentviews=1355)
- * thanks Will.
+ * bootloader.c taken from krasutski.denis https://my.st.com/public/STe2ecommunities/mcu/Lists/cortex_mx_stm32/Flat.aspx?RootFolder=%2Fpublic%2FSTe2ecommunities%2Fmcu%2FLists%2Fcortex_mx_stm32%2FJump%20to%20USB%20DFU%20Bootloader%20in%20startup%20code%20on%20STM32F042&FolderCTID=0x01200200770978C69A1141439FE559EB459D7580009C4E14902C3CDE46A77F0FFD06506F5B&currentviews=2147
+ * Thanks Denis!
  */
 
 #include "stm32f0xx_hal.h"
 #include "usb_device.h"
 
-
-
-
-#define BOOTLOADER_MAGIC_TOKEN 0xDEADBEEF  // :D
-volatile uint32_t* BOOTLOADER_MAGIC_ADDR = ((uint32_t*) ((uint32_t) 0x20001000)); //4k into SRAM (out of 6k)
-//Value taken from CD00167594.pdf page 35, system memory start.
-#define BOOTLOADER_START_ADDR 0x1fffc400 //for ST32F042
-
-
-
-void RebootToBootloader(){
 //call this at any time to initiate a reboot into bootloader
+void RebootToBootloader(){
 
-  *BOOTLOADER_MAGIC_ADDR = BOOTLOADER_MAGIC_TOKEN;
-    NVIC_SystemReset();
+
+    FLASH_OBProgramInitTypeDef OBParam;
+
+    HAL_FLASHEx_OBGetConfig(&OBParam);
+
+    OBParam.OptionType = OPTIONBYTE_USER;
+    /*Reset NBOOT0 and BOOT_SEL,  see: RM 2.5 Boot configuration*/
+    OBParam.USERConfig = 0x77; //Sorry for magic number :)
+
+    HAL_FLASH_Unlock();
+    HAL_FLASH_OB_Unlock();
+
+    HAL_FLASHEx_OBErase();
+
+    HAL_FLASHEx_OBProgram(&OBParam);
+
+    HAL_FLASH_OB_Lock();
+    HAL_FLASH_Lock();
+
+    HAL_FLASH_OB_Launch();
 }
 
-//This is the first thing the micro runs after startup_stm32f0xx.s
-//Only the SRAM is initialized at this point
+//turns off BOOT0 pin
 void bootloaderSwitcher(){
-  uint32_t jumpaddr,tmp;
-  GPIO_InitTypeDef GPIO_InitStruct;
+    FLASH_OBProgramInitTypeDef OBParam;
 
-  tmp=*BOOTLOADER_MAGIC_ADDR;
-  //tmp=BOOTLOADER_MAGIC_TOKEN; //DEBUG
-  if (tmp == BOOTLOADER_MAGIC_TOKEN){
-    *BOOTLOADER_MAGIC_ADDR=0; //Zero it so we don't loop by accident
+    HAL_FLASHEx_OBGetConfig(&OBParam);
 
-    //For the LQFP48 of STM32F042, the BOOT0 pin is at PF11
-    __HAL_RCC_GPIOF_CLK_ENABLE()
-    	;
+    if(OBParam.USERConfig != 0x7F)
+    {
 
-//	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_11, GPIO_PIN_RESET);
+        OBParam.OptionType = OPTIONBYTE_USER;
+        OBParam.USERConfig = 0x7F;
 
-	/*Configure GPIO pin : CAN_MOD_Pin */
-	GPIO_InitStruct.Pin = GPIO_PIN_11;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
-
-    void (*bootloader)(void) = 0; //Zero the function pointer.
-    jumpaddr = *(__IO uint32_t*)(BOOTLOADER_START_ADDR + 4);
-    bootloader = (void (*)(void)) jumpaddr; //Set the function pointer to bootaddr +4
-    __set_MSP(*(__IO uint32_t*) BOOTLOADER_START_ADDR); //load the stackpointer - bye bye program
-    bootloader(); //GO TO DFU MODE MOFO :D
-
-    //this should never be hit, trap for debugging
-    while(1){}
-  }
+        HAL_FLASH_Unlock();
+        HAL_FLASH_OB_Unlock();
+        HAL_FLASHEx_OBErase();
+        HAL_FLASHEx_OBProgram(&OBParam);
+        HAL_FLASH_OB_Lock();
+        HAL_FLASH_OB_Launch();
+    }
 }
 
